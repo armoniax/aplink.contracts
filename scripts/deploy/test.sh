16117851a9102a41bc8be2f29294a010c9f072b6
Joss <281900360@qq.com>
2022-03-06 21:09:01 +0800

    add shell script

--- scripts/deploy/test.sh
+++ scripts/deploy/test.sh
@@ -6,8 +6,8 @@ createAccount() {
     ret=`ssh sh-misc "${remoteDockerScrip} '${createAccountScript}'"`
     echo $ret
     #get priv key 
-    privKey=`echo $ret | sed -n '4,4p' | awk '{print substr($1,2)}' | sed 's/.$//'`
-    echo "privKey: ${privKey}"
+    privKey=${ret:0-54:53}
+    return 0
 }
 
 # create contract
@@ -26,29 +26,11 @@ createContract() {
 }
 
 accountTail=$1
-#build
-DOCKER_ID=amax-dev-sean
-remoteDockerScrip='docker exec -i mgp-devnet /bin/bash -c'
-buildScript='cd /opt/data/build && rm -rf ./* && cmake .. && make -j8'
-docker exec -it $DOCKER_ID /bin/bash -c ${buildScript}
-
-#scp
-remoteContractPath=/mnt/data/mgp-test/eosio-docker/node_devnet/data/otccontract
-scp build/contracts/otcbook/otcbook* sh-misc:${remoteContractPath}
-scp build/contracts/otcconf/otcconf* sh-misc:${remoteContractPath}
-
 
 otcFileName='otcbook'
 #create account
 otcContractName="${otcFileName}.${accountTail}"
-createAccount otcContractName
-
-#deploy contract
-createContract ${otcFileName} ${otcContractName}
-
-otcConfName='otcconf'
-otcContractName="${otcConfName}.${accountTail}"
-
-
-#sh-misc target
-#/mnt/data/mgp-test/eosio-docker/node_devnet/data/otccontract
+remoteDockerScrip='docker exec -i mgp-devnet /bin/bash -c'
+echo $otcContractName
+createAccount $otcContractName
+echo "privKey: $privKey"
