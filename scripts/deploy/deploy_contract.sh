ec9a8fccc2ca89c158c77645b3a7aa702466716d
Joss <281900360@qq.com>
2022-03-06 18:54:44 +0800

    add run script

--- scripts/deploy/deploy_contract.sh
+++ scripts/deploy/deploy_contract.sh
@@ -1,1 +1,19 @@
-s
+
+#build
+DOCKER_ID=amax-dev-sean
+buildScript='cd /opt/data/build && rm -rf ./* && cmake .. && make -j8'
+docker exec -it $DOCKER_ID /bin/bash -c ${buildScript}
+
+#scp
+remoteContractPath=/mnt/data/mgp-test/eosio-docker/node_devnet/data/otccontract
+scp build/contracts/otcbook/otcbook* sh-misc:${remoteContractPath}
+scp build/contracts/otcconf/otcconf* sh-misc:${remoteContractPath}
+
+#deploy
+remoteDockerScrip='docker exec -i mgp-devnet /bin/bash -c '
+unlockScript='cleos wallet unlock --password PW5KQzzoYJcijs2wtMpF5Vqk4v8n9FNcxxHj1aqqcjpGJDEkdBrog'
+ssh sh-misc "docker exec -i mgp-devnet /bin/bash -c '${unlockScript}'"
+
+
+#sh-misc target
+#/mnt/data/mgp-test/eosio-docker/node_devnet/data/otccontract
