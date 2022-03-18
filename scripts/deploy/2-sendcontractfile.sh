2ea2b454de87ded3b01e0d7c9e101e26bb120254
Joss <281900360@qq.com>
2022-03-18 14:58:04 +0800

    update

---
+++ scripts/deploy/2-sendcontractfile.sh
@@ -0,0 +1,17 @@
+
+#build
+buildContract(){
+    DOCKER_ID=amax-dev-sean
+    buildScript='cd /opt/data/build && rm -rf ./* && cmake .. && make -j8'
+    docker exec -i $DOCKER_ID /bin/bash -c "${buildScript}"
+}
+
+#scp
+scpToMiscMerchine(){
+    remoteContractPath=/mnt/data/mgp-test/eosio-docker/node_devnet/data/otccontract
+    scp build/contracts/otcbook/otcbook* sh-misc:${remoteContractPath}
+    scp build/contracts/otcconf/otcconf* sh-misc:${remoteContractPath}
+}
+
+buildContract
+scpToMiscMerchine
