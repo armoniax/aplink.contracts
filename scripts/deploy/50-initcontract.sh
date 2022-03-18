2d520af184443792856819844346f2677ad47d70
Joss <281900360@qq.com>
2022-03-18 20:13:08 +0800

    update

--- scripts/deploy/50-initcontract.sh
+++ scripts/deploy/50-initcontract.sh
@@ -1,5 +1,10 @@
+unlock() {
+  unlock="cleos wallet unlock --password PW5KQzzoYJcijs2wtMpF5Vqk4v8n9FNcxxHj1aqqcjpGJDEkdBrog"
+   ssh sh-misc "${remoteDockerScrip} '${unlock}'"
+}
+
 initContract() {
-  updateContract='cleos push action ${otcAccountName} init "[\"$confAccountName\"]" -p \"${otcAccountName}@active\"'
+  updateContract="cleos push action ${confAccountName} init '[]' -p  ${confAccountName}@active"
   echo "-------initContract--------------"
   ssh sh-misc "${remoteDockerScrip} '${updateContract}'"
 }
@@ -14,5 +19,6 @@ remoteDockerScrip='docker exec -i mgp-devnet /bin/bash -c'
 otcAccountName=${1}.o
 confAccountName=${1}.h
 
+unlock
 initContract 
 setPermission
