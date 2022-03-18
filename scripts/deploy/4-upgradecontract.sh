6aec0569fb11e5ee1e97de5d5ffbd851025a008b
Joss <281900360@qq.com>
2022-03-18 15:12:25 +0800

    update

--- scripts/deploy/4-upgradecontract.sh
+++ scripts/deploy/4-upgradecontract.sh
@@ -1,9 +1,11 @@
 updateContract(){
-  contractName=$1
   contractFilePath='/opt/mgp/node_devnet/data/otccontract'
-  updateContract="cleos set contract ${contractName} ${contractFilePath} ${otcFileName}.wasm ${otcFileName}.abi -p ${contractName}@active"
+  updateContract="cleos set contract ${accountName} ${contractFilePath} ${otcFileName}.wasm ${otcFileName}.abi -p ${accountName}@active"
   ssh sh-misc "${remoteDockerScrip} '${updateContract}'"
 }
-contractName=$1
+
+accountName=$1
 otcFileName=$2
 remoteDockerScrip='docker exec -i mgp-devnet /bin/bash -c'
+
+updateContract 
