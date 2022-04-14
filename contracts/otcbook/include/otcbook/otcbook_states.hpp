3a00e4a1aab02eee3b0d0d95b4715c4ccbacd3bb
Jason <jason.chen@amax.com>
2022-04-14 23:17:05 +0800

    uint64_t

--- contracts/otcbook/include/otcbook/otcbook_states.hpp
+++ contracts/otcbook/include/otcbook/otcbook_states.hpp
@@ -139,13 +139,13 @@ struct OTCBOOK_TBL merchant_t {
 
     uint64_t primary_key()const { return owner.value; }
     uint64_t scope()const { return 0; }
-    uint128_t by_update_time() const {
-        return (uint128_t) updated_at.utc_seconds ;
+    uint64_t by_update_time() const {
+        return (uint64_t) updated_at.utc_seconds ;
     }
 
     typedef eosio::multi_index<"merchants"_n, merchant_t,
         indexed_by<"status"_n, const_mem_fun<merchant_t, uint64_t, &merchant_t::by_state> >,
-        indexed_by<"updatedat"_n, const_mem_fun<merchant_t, uint128_t, &merchant_t::by_update_time> >
+        indexed_by<"updatedat"_n, const_mem_fun<merchant_t, uint64_t, &merchant_t::by_update_time> >
     > idx_t;
 
     EOSLIB_SERIALIZE(merchant_t,  (owner)(merchant_name)(merchant_detail)
@@ -215,8 +215,8 @@ struct OTCBOOK_TBL order_t {
         return can_be_taken() ? (uint128_t)va_quantity.symbol.code().raw() << 64 | va_price.amount
                     : std::numeric_limits<uint128_t>::max();
     }
-    uint128_t by_update_time() const {
-        return (uint128_t) updated_at.utc_seconds << 64 | id;
+    uint64_t by_update_time() const {
+        return (uint64_t) updated_at.utc_seconds << 64 ;
     }
   
     EOSLIB_SERIALIZE(order_t,   (id)(owner)(merchant_name)(accepted_payments)(va_price)(va_quantity)
@@ -235,7 +235,7 @@ typedef eosio::multi_index
     indexed_by<"price"_n, const_mem_fun<order_t, uint64_t, &order_t::by_invprice> >,
     indexed_by<"maker"_n, const_mem_fun<order_t, uint128_t, &order_t::by_maker_status> >,
     indexed_by<"coin"_n, const_mem_fun<order_t, uint128_t, &order_t::by_coin> >,
-    indexed_by<"updatedat"_n, const_mem_fun<order_t, uint128_t, &order_t::by_update_time> >
+    indexed_by<"updatedat"_n, const_mem_fun<order_t, uint64_t, &order_t::by_update_time> >
 > buy_order_table_t;
 
 /**
@@ -248,7 +248,7 @@ typedef eosio::multi_index
     indexed_by<"price"_n, const_mem_fun<order_t, uint64_t, &order_t::by_price> >,
     indexed_by<"maker"_n, const_mem_fun<order_t, uint128_t, &order_t::by_maker_status> >,
     indexed_by<"coin"_n, const_mem_fun<order_t, uint128_t, &order_t::by_coin> >,
-    indexed_by<"updatedat"_n, const_mem_fun<order_t, uint128_t, &order_t::by_update_time> >
+    indexed_by<"updatedat"_n, const_mem_fun<order_t, uint64_t, &order_t::by_update_time> >
 > sell_order_table_t;
 
 
@@ -359,8 +359,8 @@ struct OTCBOOK_TBL deal_t {
     uint128_t by_coin() const {
         return (uint128_t)deal_quantity.symbol.code().raw() << 64 | order_price.amount;
     }
-    uint128_t by_update_time() const {
-        return (uint128_t) updated_at.utc_seconds << 64 | id;
+    uint64_t by_update_time() const {
+        return (uint64_t) updated_at.utc_seconds << 64 ;
     }
     // uint64_t by_expired_at() const    { return uint64_t(expired_at.sec_since_epoch()); }
     // uint64_t by_maker_expired_at() const    { return uint64_t(maker_expired_at.sec_since_epoch()); }
@@ -374,7 +374,7 @@ struct OTCBOOK_TBL deal_t {
         indexed_by<"ordersn"_n, const_mem_fun<deal_t, uint64_t, &deal_t::by_ordersn> >,
         indexed_by<"orderid"_n, const_mem_fun<deal_t, uint128_t, &deal_t::by_order_id> >,
         indexed_by<"coin"_n,    const_mem_fun<deal_t, uint128_t, &deal_t::by_coin> >,
-        indexed_by<"updatedat"_n, const_mem_fun<deal_t, uint128_t, &deal_t::by_update_time> >
+        indexed_by<"updatedat"_n, const_mem_fun<deal_t, uint64_t, &deal_t::by_update_time> >
         // indexed_by<"expiry"_n,  const_mem_fun<deal_t, uint64_t, &deal_t::by_expired_at> >
     > idx_t;
 
@@ -412,14 +412,14 @@ struct OTCBOOK_TBL fund_log_t {
         return (uint128_t)action.value << 64 | owner.value;
     }
     uint128_t by_update_time() const {
-        return (uint128_t) updated_at.utc_seconds << 64 | id;
+        return (uint64_t) updated_at.utc_seconds << 64 ;
     }
 
     typedef eosio::multi_index
     <"fundlog"_n, fund_log_t,
         indexed_by<"owner"_n,   const_mem_fun<fund_log_t, uint64_t, &fund_log_t::by_owner> > ,
         indexed_by<"action"_n,   const_mem_fun<fund_log_t, uint128_t, &fund_log_t::by_action> >,
-        indexed_by<"updatedat"_n, const_mem_fun<fund_log_t, uint128_t, &fund_log_t::by_update_time> >
+        indexed_by<"updatedat"_n, const_mem_fun<fund_log_t, uint64_t, &fund_log_t::by_update_time> >
     > table_t;
 
     EOSLIB_SERIALIZE(fund_log_t,    (id)(owner)(order_id)(order_side)(action)(quantity)(log_at)(updated_at) )
