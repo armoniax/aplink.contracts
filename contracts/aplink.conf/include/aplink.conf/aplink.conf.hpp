60753173e4c979f8cf8e7099d7cdda1fa179db6d
masteray <masteraychen@gmail.com>
2022-05-12 14:21:56 +0800

    add price into aplink.conf

--- contracts/aplink.conf/include/aplink.conf/aplink.conf.hpp
+++ contracts/aplink.conf/include/aplink.conf/aplink.conf.hpp
@@ -10,6 +10,7 @@ namespace aplink {
 
 using std::string;
 using std::vector;
+using std::pair;
 using namespace eosio;
 
 static constexpr symbol   SYS_SYMBOL            = symbol(symbol_code("AMAX"), 8);
@@ -17,14 +18,9 @@ static constexpr symbol   APL_SYMBOL            = symbol(symbol_code("APL"), 4);
 static constexpr symbol   CNYD_SYMBOL           = symbol(symbol_code("CNYD"), 4);
 
 struct account_res {
-    uint8_t account_create_ram_bytes    = (uint8_t) 4096;
-    asset account_stake_cpu             = asset(200000, SYS_SYMBOL);
-    asset account_stake_net             = asset(200000, SYS_SYMBOL);
-};
-
-struct price_info {
-    symbol_code symb;
-    asset price;
+    uint8_t account_create_ram_bytes            = (uint8_t) 4096;
+    asset account_stake_cpu                     = asset(200000, SYS_SYMBOL);
+    asset account_stake_net                     = asset(200000, SYS_SYMBOL);
 };
 
 /**
@@ -49,28 +45,37 @@ public:
     ~settings() { _global.set( _gstate, get_self() ); }
    
     ACTION init(const name& admin);
-    using init_action = eosio::action_wrapper<"init"_n, &settings::init>;
-
-    ACTION update(const account_res& account_create_res);
-    using update_action = eosio::action_wrapper<"update"_n, &settings::update>;
+    ACTION setacctres(const account_res& account_create_res);
+    ACTION setprices(const vector<pair<symbol_code, asset>> prices);
 
 private:
     struct [[eosio::table("global"), eosio::contract("aplink.conf")]] global_t {
         name admin;     //default: _self
         account_res account_create_res;
         vector<string> amc_nodes =  {
-            "a1.nchain.me:8888",
-            "a2.nchain.me:8888",
+            "amc1.nchain.me:8888",
+            "amc2.nchain.me:8888",
         };
-        map<price_info> prices = {
-            { SYS_SYMBOL.code(), asset(100, CNYD_SYMBOL) },
-            { APL_SYMBOL.code(), asset(1, CNYD_SYMBOL) } 
-        };
+        bool show_price = false;
         
-        EOSLIB_SERIALIZE( global_t, (admin)(account_create_res)(amc_nodes)(prices) )
+        EOSLIB_SERIALIZE( global_t, (admin)(account_create_res)(amc_nodes)(show_price) )
     };
-
     typedef eosio::singleton< "global"_n, global_t > global_singleton;
+
+    struct [[eosio::table("prices"), eosio::contract("aplink.conf")]] price_t {
+        symbol_code         symb;
+        asset               price;
+        time_point_sec      updated_at;
+
+        uint64_t    primary_key()const { return symb.raw(); }
+
+        price_t() {}
+        price_t(const symbol_code& sc, const asset& p): symb(sc), price(p) { updated_at = time_point_sec(current_time_point()); }
+
+        EOSLIB_SERIALIZE( price_t, (symb)(price)(updated_at) )
+
+        typedef eosio::multi_index<"prices"_n, price_t > tbl_t;
+    };
 
     global_singleton    _global;
     global_t            _gstate;
--- contracts/aplink.conf/src/aplink.conf.cpp
+++ contracts/aplink.conf/src/aplink.conf.cpp
@@ -2,6 +2,8 @@
 
 namespace aplink {
 
+using namespace std;
+
 ACTION settings::init(const name& admin) {
    require_auth( _self );
    
@@ -9,12 +11,35 @@ ACTION settings::init(const name& admin) {
    _global.remove();
 }
 
-ACTION settings::update(const account_res& account_create_res) {
+ACTION settings::setacctres(const account_res& account_create_res) {
    check( is_account(_gstate.admin), "uninitalized" );
    require_auth( _gstate.admin );
    
    _gstate.account_create_res = account_create_res;
 
+}
+
+ACTION settings::setprices(const vector<pair<symbol_code, asset>> prices) {
+   require_auth( _gstate.admin );
+
+   auto priceidx = price_t::tbl_t(_self, _self.value);
+   
+   for (auto& price_info : prices) {
+      auto itr = priceidx.find(price_info.first.raw());
+      if (itr == priceidx.end()) {
+         priceidx.emplace(_self, [&](auto& rec){
+            rec.symb = price_info.first;
+            rec.price = price_info.second;
+            rec.updated_at = time_point_sec(current_time_point());
+         });
+
+      } else {
+         priceidx.modify(itr, _self, [&](auto& rec){
+            rec.price = price_info.second;
+            rec.updated_at = time_point_sec(current_time_point());
+         });
+      }
+   }
 }
 
 } /// namespace aplink
