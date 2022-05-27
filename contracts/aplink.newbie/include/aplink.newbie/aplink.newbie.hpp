9e7c8ceddec85aa99964b524103c4ce56f13aa61
masteray <masteraychen@gmail.com>
2022-05-27 18:04:59 +0800

    add aplink farm

--- contracts/aplink.newbie/include/aplink.newbie/aplink.newbie.hpp
+++ contracts/aplink.newbie/include/aplink.newbie/aplink.newbie.hpp
@@ -6,10 +6,14 @@
 #include <eosio/time.hpp>
 
 #include "aplink.newbie.db.hpp"
+#include "utils.hpp"
+
 // #include "wasm_db.hpp"
 
 using namespace eosio;
 using namespace wasm::db;
+
+// static constexpr aplink_token_contract = "aplink.token"_n;
 
 class [[eosio::contract("aplink.newbie")]] newbie : public contract {
 private:
@@ -32,11 +36,11 @@ public:
         _global.set( _gstate, get_self() );
     }
 
-    [[eosio::action]]
-    void claimreward(const name& newbie);
+    ACTION claimreward(const name& newbie);
 
-    [[eosio::action]]
-    void setstate(const bool& enable, const asset& newbie_reward, const name& contract_name);
+    ACTION rewardinvite(const name& to);
+
+    ACTION setstate(const bool& enable, const asset& newbie_reward, const name& aplink_token_contract);
 
     /**
      * recycle db
