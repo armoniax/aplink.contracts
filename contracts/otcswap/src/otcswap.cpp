f5768cf67530d53f422d6d3cb4dfa8b1e4de5940
narsiss <vvwyot@126.com>
2022-06-30 18:14:42 +0800

    fix swap bug

--- contracts/otcswap/src/otcswap.cpp
+++ contracts/otcswap/src/otcswap.cpp
@@ -53,7 +53,7 @@ void otcswap::settleto(const name &user, const asset &fee, asset quantity)
     require_auth(_conf().managers.at(otc::manager_type::otcbook));
     check(quantity.amount > 0, "quantity must be positive");
     check(fee.amount > 0, "quantity must be positive");
-    check(STAKE_USDT==quantity.symbol, "only support settling for MUSDT";
+    check(STAKE_USDT == quantity.symbol, "only support settling for MUSDT");
     check(is_account(user), "owner account does not exist");
     auto sym = quantity.symbol;
     auto account = account_t(user);
