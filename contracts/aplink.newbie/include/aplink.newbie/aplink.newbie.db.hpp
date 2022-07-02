c51b0ad026615d207abda4b3ebe65697a117bd20
masteray <masteraychen@gmail.com>
2022-07-02 22:41:42 +0800

    format

--- contracts/aplink.newbie/include/aplink.newbie/aplink.newbie.db.hpp
+++ contracts/aplink.newbie/include/aplink.newbie/aplink.newbie.db.hpp
@@ -64,15 +64,11 @@ typedef eosio::singleton< "global"_n, global_t > global_singleton;
 // struct CUSTODY_TBL claim_t {
 //     name        claimer;
 //     time_point  claimed_at;
-
 //     uint64_t    primary_key()const { return claimer.value; }
 //     uint64_t    scope() const { return 0; }
-
 //     claim_t() {}
 //     claim_t(const name& c): claimer(c) {}
-
 //     EOSLIB_SERIALIZE( claim_t, (claimer)(claimed_at) )
-
 //     typedef eosio::multi_index<"claims"_n, claim_t > tbl_t;
 // };
 
