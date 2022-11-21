78cad0fb82d14c3809d880b5ddb13ee653b7a5ff
masteray <masteraychen@gmail.com>
2022-11-21 16:36:51 +0800

    add setlease bin

---
+++ scripts/setleases.sh
@@ -0,0 +1,14 @@
+lid=7
+app_url=""
+bg_url=https://xdao.mypinata.cloud/ipfs/QmQstPefzohuZQSkFwGxvca5cLAEMGSCakicokjKUxJ3Au
+mpush aplink.farm setlease '['$lid',"'$app_url'","'$bg_url'"]' -parmoniaadmin
+
+lid=4
+app_url=https://m.nftone.life
+bg_url=https://xdao.mypinata.cloud/ipfs/QmcevuHniX6dveqQR1oeUQ9iwu1pcbHS8d5up9h3kqApw2
+mpush aplink.farm setlease '['$lid',"'$app_url'","'$bg_url'"]' -parmoniaadmin
+
+lid=5
+app_url=https://did.aplink.app
+bg_url=https://xdao.mypinata.cloud/ipfs/QmeXHYwq7YvEWmWNcjm7ZP89JyUhJrggUVuNiYHkEXn43q
+mpush aplink.farm setlease '['$lid',"'$app_url'","'$bg_url'"]' -parmoniaadmin
